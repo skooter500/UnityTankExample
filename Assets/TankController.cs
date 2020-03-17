5b2431283a449fdf378b5748ec9f5d85efc6bd59
Bryan Duggan <skooter500@gmail.com>
2020-03-17 19:28:50 +0000

    bla

---
+++ Assets/TankController.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TankController : MonoBehaviour
+{
+    [Range(0.0f, 20.0f)]
+    public float speed = 5.0f;
+
+    [Range(0.0f, 360.0f)]
+    public float rotSpeed = 5.0f;
+
+    public GameObject bulletPrefab;
+    public Transform bulletSpawnPoint;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(0, 0, Input.GetAxis("Vertical") * speed * Time.deltaTime);
+        transform.Rotate(0, rotSpeed * Input.GetAxis("Horizontal") * Time.deltaTime, 0);
+
+        if (Input.GetKeyDown(KeyCode.Joystick1Button0) || Input.GetKeyDown(KeyCode.Space))
+        {
+            GameObject bullet = GameObject.Instantiate<GameObject>(bulletPrefab
+                ,bulletSpawnPoint.position
+                ,bulletSpawnPoint.rotation
+                );
+        }
+    }
+}
---
+++ Assets/Wall.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Wall : MonoBehaviour
+{
+    public int width;
+    public int height;
+    // Start is called before the first frame update
+    void Start()
+    {
+        for (int row = 0; row < height; row++)
+        {
+            for (int col = 0; col < width; col++)
+            {
+                GameObject cube = GameObject.CreatePrimitive(PrimitiveType.Cube);
+                Vector3 pos = new Vector3(col, row, 0);
+                pos = transform.TransformPoint(pos);
+                cube.transform.position = pos;
+                cube.transform.rotation = transform.rotation;
+                cube.GetComponent<Renderer>().material.color = Color.HSVToRGB(Random.Range(0.0f, 1.0f), 1, 1);
+                cube.AddComponent<Rigidbody>();
+                cube.transform.parent = this.transform;
+            }
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
